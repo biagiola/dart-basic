b26017a037738d05c1a8fd7a49438792f251bb80
David <41311088+biagiola@users.noreply.github.com>
2019-09-28 12:53:48 -0400

    Add files via upload

---
+++ 01-variables.dart
@@ -0,0 +1,23 @@
+void main(){
+  // Static string
+  String user1 = 'julian';
+  print(user1);
+  
+  // dynamic string
+  dynamic user2 = 'marcelo';
+  print(user2);
+  
+  // int's are dynamic
+  int number = 55;
+  number = 66;
+  print(number);
+  
+  // Array
+  List users1 = ['antonio', 'Muller', 'John', 77];
+  users1.add(13);
+  users1.remove('antonio');
+  print(users1);
+  // Speficy type of data
+  //List<int> users1 = ['antonio', 'Muller', 'John'];
+  
+}
